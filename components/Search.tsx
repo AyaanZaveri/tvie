aabd4c46b58f3a9515c3c6b3865a8bcc9a393704
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-12 11:07:22 -0500

    Changing input

--- components/Search.tsx
+++ components/Search.tsx
@@ -9,11 +9,11 @@ interface SearchProps {
 const Search = ({ handleSearch, query, setQuery }: SearchProps) => {
   return (
     <div className="flex w-screen flex-row justify-start bg-neutral-800 p-5 font-bold shadow-xl">
-      <div className='ml-16'>
+      <div className='ml-8'>
         <form className="w-96" onSubmit={handleSearch}>
           <input
             type="text"
-            className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            className="block w-full py-2 px-4 rounded-md border-neutral-300 shadow-sm focus:ring focus:ring-amber-400 outline-none"
             placeholder="Search..."
             value={query}
             onChange={(e) => setQuery(e.target.value)}
