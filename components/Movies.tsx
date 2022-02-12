0eb0ccfc8c53fddbf85fbf5913080dc9011d3797
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-12 17:38:43 -0500

    Favorites implemented

--- components/Movies.tsx
+++ components/Movies.tsx
@@ -12,7 +12,7 @@ interface MovieProps {
   id: number
 }
 
-const Movies = ({ movie }: { movie: MovieProps }) => {
+const Movies = ({ movie, filter }: { movie: MovieProps; filter: boolean }) => {
   const [favorited, setFavorited] = useState<boolean>(false)
 
   const longDate = (date: any) => {
@@ -29,34 +29,38 @@ const Movies = ({ movie }: { movie: MovieProps }) => {
     : console.log('Unfavorited: ' + movie.id)
 
   return (
-    <div className="group relative rounded-lg shadow-2xl backdrop-brightness-0">
-      <img
-        className="w-80 rounded-lg shadow-sm"
-        src={
-          movie.backdrop_path
-            ? `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`
-            : 'https://via.placeholder.com/1280x720'
-        }
-      />
-      <div
-        onClick={() => setFavorited(!favorited)}
-        className={`absolute top-0 right-0 m-1 rounded-lg p-2 ${
-          favorited ? 'text-amber-400' : 'text-slate-100'
-        } backdrop-blur delay-150 hover:cursor-pointer hover:text-amber-400 transition-colors`}
-      >
-        <HiStar />
-      </div>
-      <div className="absolute bottom-0 left-0 flex w-full flex-row justify-between rounded-lg bg-slate-50 bg-opacity-10 py-3 px-4 shadow-2xl backdrop-blur transition-all group-hover:py-4">
-        <div className="flex flex-col">
-          <span className="font-medium text-slate-200">{movie.title}</span>
-          <span className="text-sm font-light text-slate-300">
-            {longDate(movie.release_date)}
+    <div
+      className={`${filter ? `${favorited ? "block" : "hidden"}` : "block"} group relative rounded-lg shadow-2xl backdrop-brightness-0`}
+    >
+      <div>
+        <img
+          className="w-80 rounded-lg shadow-sm"
+          src={
+            movie.backdrop_path
+              ? `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`
+              : 'https://via.placeholder.com/1280x720'
+          }
+        />
+        <div
+          onClick={() => setFavorited(!favorited)}
+          className={`absolute top-0 right-0 m-1 rounded-lg p-2 ${
+            favorited ? 'text-amber-400' : 'text-slate-100'
+          } backdrop-blur transition-colors delay-150 hover:cursor-pointer hover:text-amber-400`}
+        >
+          <HiStar />
+        </div>
+        <div className="absolute bottom-0 left-0 flex w-full flex-row justify-between rounded-lg bg-slate-50 bg-opacity-10 py-3 px-4 shadow-2xl backdrop-blur transition-all group-hover:py-4">
+          <div className="flex flex-col">
+            <span className="font-medium text-slate-200">{movie.title}</span>
+            <span className="text-sm font-light text-slate-300">
+              {longDate(movie.release_date)}
+            </span>
+          </div>
+
+          <span className="h-min rounded-sm px-1.5 text-sm text-slate-200 ring-1 ring-slate-300">
+            <CountUp end={movie.vote_average} duration={1} decimals={1} />
           </span>
         </div>
-
-        <span className="h-min rounded-sm px-1.5 text-sm text-slate-200 ring-1 ring-slate-300">
-          <CountUp end={movie.vote_average} duration={1} decimals={1} />
-        </span>
       </div>
     </div>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,9 @@ import Search from '../components/Search'
 const Home = () => {
   const [movies, setMovies] = useState<any>()
   const [query, setQuery] = useState<string>('')
+  const [filter, setFilter] = useState<boolean>(false)
+
+  console.log(filter)
 
   const POPULAR_MOVIE_API =
     'https://api.themoviedb.org/4/discover/movie?api_key=cbf7e1167c533eaa4ed56af5cd8aeb85&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate'
@@ -41,11 +44,20 @@ const Home = () => {
       <Search handleSearch={handleSearch} query={query} setQuery={setQuery} />
       <div className="mt-24 flex flex-col items-center justify-center md:items-start md:justify-start">
         {/* <span className='text-white font-bold text-5xl'>ToVi</span> */}
-      <h1 className="md:ml-12 text-4xl font-bold text-slate-100">Movies</h1>
+        <h1 className="text-4xl font-bold text-slate-100 md:ml-12">Movies</h1>
+        <div className='inline-flex gap-2 items-center text-white'>
+          <span>Show Favorites</span>
+          <input
+            type="checkbox"
+            onChange={() => setFilter(!filter)}
+          />
+        </div>
       </div>
       <div className="mt-6 flex flex-row flex-wrap items-center justify-center gap-5 md:ml-12 md:items-start md:justify-start">
         {movies
-          ? movies.map((movie: any) => <Movies key={movie.id} movie={movie} />)
+          ? movies.map((movie: any) => (
+              <Movies key={movie.id} movie={movie} filter={filter} />
+            ))
           : null}
       </div>
     </div>
