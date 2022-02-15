4393535f4e6db5952d21274f77251f7d1ca9a298
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-15 16:08:55 -0500

    Star

--- components/MovieInfo.tsx
+++ components/MovieInfo.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import CountUp from 'react-countup'
+import { HiStar } from 'react-icons/hi'
 
 interface MovieInfoProps {
   title: string
@@ -7,11 +8,17 @@ interface MovieInfoProps {
   original_language: string
   release_date: string
   vote_average: number
-  id: number,
+  id: number
   overview: string
 }
 
-const MovieInfo = ({ movieData }: { movieData: MovieInfoProps }) => {
+interface Props {
+  movieData: MovieInfoProps
+  handleFavorite: () => void
+  favorited: boolean
+}
+
+const MovieInfo = ({ movieData, handleFavorite, favorited }: Props ) => {
   return (
     <div className="flex h-screen flex-wrap items-center justify-center">
       <div className="flex flex-row items-start gap-5">
@@ -23,7 +30,7 @@ const MovieInfo = ({ movieData }: { movieData: MovieInfoProps }) => {
               : 'https://via.placeholder.com/1280x720'
           }
         />
-        <div className='flex flex-col gap-5'>
+        <div className="flex flex-col gap-5">
           <div className="flex flex-row items-center gap-3">
             <span className="text-3xl font-bold text-slate-100">
               {movieData.title}
@@ -31,8 +38,16 @@ const MovieInfo = ({ movieData }: { movieData: MovieInfoProps }) => {
             <span className="h-min rounded-sm px-1.5 text-sm text-slate-200 ring-1 ring-slate-300">
               <CountUp end={movieData.vote_average} duration={1} decimals={1} />
             </span>
+            <div
+              onClick={handleFavorite}
+              className={`absolute top-0 right-0 m-1 rounded-lg p-2 ${
+                favorited ? 'text-amber-400' : 'text-slate-100'
+              } backdrop-blur transition-colors delay-150 hover:cursor-pointer hover:text-amber-400`}
+            >
+              <HiStar />
+            </div>
           </div>
-          <span className='w-[48rem] text-slate-100'>{movieData.overview}</span>
+          <span className="w-[48rem] text-slate-100">{movieData.overview}</span>
         </div>
       </div>
     </div>
