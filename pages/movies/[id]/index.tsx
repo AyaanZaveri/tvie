7c5266c8d5ef14ae8f6d09b459d56d29115c3d89
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-15 15:25:57 +0000

    Context Props

---
+++ pages/movies/[id]/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import axios from 'axios'
+
+const MovieIndex = ({data} : {data:any}) => {
+  return (
+    <div></div>
+  )
+}
+
+export const getServerSideProps = async (context:any) => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${context.params.id}/?api_key=cbf7e1167c533eaa4ed56af5cd8aeb85&language=en-US`
+    );
+  
+    return {
+      props: {
+        data,
+      },
+    };
+  };
+
+export default MovieIndex
