fb69af2a6fb01fad3465bbc3c03c454936ef2789
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-15 18:20:48 +0000

    MovieInfo

--- components/MovieInfo.tsx
+++ components/MovieInfo.tsx
@@ -1,17 +1,39 @@
 import React from 'react'
+import CountUp from 'react-countup'
 
 interface MovieInfoProps {
   title: string
-  backdrop_path: string
+  poster_path: string
   original_language: string
   release_date: string
   vote_average: number
   id: number
 }
 
-const MovieInfo = ({movieData} : {movieData: MovieInfoProps}) => {
+const MovieInfo = ({ movieData }: { movieData: MovieInfoProps }) => {
   return (
-    <div>MovieInfo</div>
+    <div className="mr-96 flex h-screen flex-wrap items-center justify-center">
+      <div className="flex flex-row items-start gap-5">
+        <img
+          className="w-64 rounded-lg shadow-2xl brightness-110 transition-all hover:brightness-125"
+          src={
+            movieData.poster_path
+              ? `https://image.tmdb.org/t/p/w1280${movieData.poster_path}`
+              : 'https://via.placeholder.com/1280x720'
+          }
+        />
+        <div className='flex flex-col gap-5'>
+          <div className="flex flex-row items-center gap-3">
+            <span className="text-3xl font-bold text-slate-100">
+              {movieData.title}
+            </span>
+            <span className="h-min rounded-sm px-1.5 text-sm text-slate-200 ring-1 ring-slate-300">
+              <CountUp end={movieData.vote_average} duration={1} decimals={1} />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- pages/movie/[id]/index.tsx
+++ pages/movie/[id]/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import axios from 'axios'
 import Movies from '../../../components/Movies'
+import MovieInfo from '../../../components/MovieInfo'
 
 const MovieIndex = ({movieData} : {movieData:any}) => {
   return (
-    <div className='w-80'><Movies movie={movieData} /></div>
+    <div><MovieInfo movieData={movieData} /></div>
   )
 }
 
