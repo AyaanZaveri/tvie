379463015abc781e5f94a4fcdfaca50213b56eb0
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-15 16:27:30 +0000

    Routes are working!

--- pages/movie/[id]/index.tsx
+++ pages/movie/[id]/index.tsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import axios from 'axios'
+import Movies from '../../../components/Movies'
 
-const MovieIndex = ({data} : {data:any}) => {
+const MovieIndex = ({movieData} : {movieData:any}) => {
   return (
-    <div>{JSON.stringify(data)}</div>
+    <div>{JSON.stringify(movieData)}</div>
   )
 }
 
 export const getServerSideProps = async (context:any) => {
-    const { data } = await axios.get(
-      `https://api.themoviedb.org/3/movie/${context.params.id}/?api_key=cbf7e1167c533eaa4ed56af5cd8aeb85&language=en-US`
+    const { data: movieData } = await axios.get(
+      `https://api.themoviedb.org/3/movie/${context.params.id}?api_key=cbf7e1167c533eaa4ed56af5cd8aeb85&language=en-US`
     );
   
     return {
       props: {
-        data,
+        movieData,
       },
     };
   };
