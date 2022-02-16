90060bcdc0810faf8f5a3a4dbdb15dbb16e3b77e
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-16 15:55:58 -0500

    Person

--- pages/person/[id]/index.tsx
+++ pages/person/[id]/index.tsx
@@ -1,31 +1,24 @@
 import React from 'react'
 import axios from 'axios'
-import Movies from '../../../components/Movies'
-import MovieInfo from '../../../components/MovieInfo'
 
-const MovieIndex = ({ movieData, castData }: { movieData: any, castData: any }) => {
+const PersonIndex = ({ personData }: { personData: any}) => {
   return (
     <div>
-      <MovieInfo movieData={movieData} castData={castData.cast} />
+      <PersonInfo movieData={personData}/>
     </div>
   )
 }
 
 export const getServerSideProps = async (context: any) => {
   const { data: movieData } = await axios.get(
-    `https://api.themoviedb.org/3/movie/${context.params.id}?api_key=cbf7e1167c533eaa4ed56af5cd8aeb85&language=en-US`
-  )
-
-  const { data: castData } = await axios.get(
-    `https://api.themoviedb.org/3/movie/${context.params.id}/credits?api_key=cbf7e1167c533eaa4ed56af5cd8aeb85&language=en-US`
+    `https://api.themoviedb.org/3/person/${context.params.id}?api_key=cbf7e1167c533eaa4ed56af5cd8aeb85&language=en-US`
   )
 
   return {
     props: {
       movieData,
-      castData,
     },
   }
 }
 
-export default MovieIndex
+export default PersonIndex
