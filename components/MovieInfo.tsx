e28135317cb9ef5dec91a58c3b8b432e23c417c5
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-15 21:50:22 -0500

    IMDB

--- components/MovieInfo.tsx
+++ components/MovieInfo.tsx
@@ -10,6 +10,7 @@ interface MovieInfoProps {
   vote_average: number
   id: number
   overview: string
+  imdb_id: string
 }
 
 interface CastInfoProps {
@@ -24,7 +25,7 @@ interface Props {
 }
 
 const MovieInfo = ({ movieData, castData }: Props) => {
-  console.log(castData)
+  console.log(movieData)
 
   const slicedCastData = castData.slice(0, 5)
 
@@ -47,9 +48,17 @@ const MovieInfo = ({ movieData, castData }: Props) => {
             <span className="h-min rounded-sm px-1.5 text-sm text-slate-200 ring-1 ring-slate-300">
               <CountUp end={movieData.vote_average} duration={1} decimals={1} />
             </span>
+            <span className="w-10">
+              <a target="_blank" href={`https://www.imdb.com/title/${movieData.imdb_id}/`}>
+                <img
+                  src="https://upload.wikimedia.org/wikipedia/commons/6/69/IMDB_Logo_2016.svg"
+                  alt=""
+                />
+              </a>
+            </span>
           </div>
           <span className="w-[48rem] text-slate-100">{movieData.overview}</span>
-          <span className='text-2xl font-bold text-slate-100'>Cast</span>
+          <span className="text-2xl font-bold text-slate-100">Cast</span>
           <div>
             <div className="flex flex-col gap-5">
               {slicedCastData
@@ -57,11 +66,11 @@ const MovieInfo = ({ movieData, castData }: Props) => {
                     <div className="flex w-full flex-row items-center break-words">
                       <img
                         src={`https://image.tmdb.org/t/p/w300_and_h300_bestv2${member.profile_path}`}
-                        className="h-12 w-12 rounded-full object-cover shadow-xl"
+                        className="h-12 w-12 rounded-full object-cover shadow-xl brightness-110 transition-all hover:brightness-125"
                         alt=""
                       />
-                      <div className='flex flex-col w-fullm ml-2'>
-                        <span className="text-sm text-slate-100 font-semibold">
+                      <div className="w-full ml-2 flex flex-col">
+                        <span className="text-sm font-semibold text-slate-100">
                           {member.name}
                         </span>
                         <span className="rounded-b-lg text-sm text-slate-300">
