d2f4f9b87f0ac6b2893bb675492712af0c484b3e
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-02-16 15:53:37 -0500

    Error page

---
+++ pages/person/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MovieRootIndex = () => {
+  return (
+    <div className="flex h-screen flex-col items-center justify-center gap-3">
+      <span className="text-6xl font-bold text-white">404</span>
+      <span className="text-lg text-white">Oops, seems like your lost...</span>
+    </div>
+  )
+}
+
+export default MovieRootIndex
